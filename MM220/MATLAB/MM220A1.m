ba6a54bc22e2fd9f6d6dd59354dd0351e325288d
Hiranmai02 <81462096+Hiranmai02@users.noreply.github.com>
2021-10-09 12:39:49 +0530

    Add files via upload

---
+++ MM220/MATLAB/MM220A1.m
@@ -0,0 +1,26 @@
+function [z] = MM220A1(xmin,deltax,xmax,ymin,deltay,ymax)
+
+%xlim([-5*pi 5*pi])
+%ylim([-5*pi 5*pi])
+xmin = -5*pi;
+ymin = -5*pi;
+xmax = 5*pi;
+ymax = 5*pi;
+deltax = pi/10;
+deltay = pi/10;
+
+x1 = [xmin:deltax:xmax];
+y1 = [ymin:deltay:ymax];
+
+[x,y] = meshgrid(x1,y1);
+
+k = sin(x).*cos(y);
+z = k./x;
+surf(x,y,z);
+
+xlabel('x');
+ylabel('y');
+zlabel('sin(x)cos(y)/x');
+
+print('MM220A1','-dpng');
+end
