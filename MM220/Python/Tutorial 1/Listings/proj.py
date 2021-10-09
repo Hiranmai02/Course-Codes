24c974b53d3d2830151527caf3f2a18ad688e96a
Hiranmai02 <81462096+Hiranmai02@users.noreply.github.com>
2021-10-09 12:33:26 +0530

    Add files via upload

---
+++ MM220/Python/Tutorial 1/Listings/proj.py
@@ -0,0 +1,39 @@
+import numpy as np
+from matplotlib.pylab import plt 
+
+def projectile(angle, v0, time):
+    t = np.linspace(0, time, 300)
+    dt = t[1] - t[0]
+    g = 9.8
+    m = 1
+
+    v0 = v0
+    vx = v0*np.cos(angle)
+    vy = v0*np.sin(angle)
+    y0 = 0
+    x0 = 0
+    vg = 0
+    y_list = [y0]
+    x_list = [x0]
+    ax = 0
+    ay = -g
+
+    for i in range(1, len(t)):
+        vg += ay*dt
+        y0 += (vg + vy)*dt
+        x0 += vx*dt
+
+        if y0 > 0:
+            y_list.append(y0)
+            x_list.append(x0)
+        else:
+            break
+
+    return x_list, y_list
+
+x_list1, y_list1 = projectile(np.pi/3, 100, 30)
+
+plt.plot(x_list1, y_list1)
+plt.xlabel("x")
+plt.ylabel("y")
+plt.show()
