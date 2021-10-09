86739f927d47b00f2b8b0622ba6471b0ecc10b50
Hiranmai02 <81462096+Hiranmai02@users.noreply.github.com>
2021-10-09 15:52:46 +0530

    Create zigzag.cpp

---
+++ C++/Lab 2/zigzag.cpp
@@ -0,0 +1,24 @@
+#include<simplecpp>
+
+main_program{
+
+int n;
+//cout<<"Enter n"<<"\n";
+cin>>n;
+
+repeat(n){
+
+repeat(5){
+cout<<"* ";
+}
+
+cout<<"\n";
+cout<<"  ";
+
+repeat(5){
+cout<<"* ";
+}
+
+cout<<"\n";
+}
+}
