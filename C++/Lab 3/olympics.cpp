1731ee15c9ad66533b69bc559b9d948a83284598
Hiranmai02 <81462096+Hiranmai02@users.noreply.github.com>
2021-10-13 10:27:43 +0530

    Create olympics.cpp

---
+++ C++/Lab 3/olympics.cpp
@@ -0,0 +1,22 @@
+#include<simplecpp>
+
+main_program{
+initCanvas();
+
+Circle c1(100,100,50);
+c1.setColor(COLOR("blue"));
+
+Circle c2(150,150,50);
+c2.setColor(COLOR("yellow"));
+
+Circle c3(210,100,50);
+c3.setColor(COLOR("black"));
+
+Circle c4(270,150,50);
+c4.setColor(COLOR("green"));
+
+Circle c5(320,100,50);
+c5.setColor(COLOR("red"));
+
+getClick();
+}
